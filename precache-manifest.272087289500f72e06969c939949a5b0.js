eb940b9e4bb12abcec59dfebb4fe8afa8dba8e9a
chiragcharania <chiragcharania@gmail.com>
2020-10-15 14:30:52 -0400

    Updates

---
+++ precache-manifest.272087289500f72e06969c939949a5b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e13713b2f9b112359c50784edcb138",
+    "url": "/MindMap/index.html"
+  },
+  {
+    "revision": "325c03f8074c54728cf9",
+    "url": "/MindMap/static/css/main.d2ff9de1.chunk.css"
+  },
+  {
+    "revision": "88c77895410f100dc2a2",
+    "url": "/MindMap/static/js/2.06fd3704.chunk.js"
+  },
+  {
+    "revision": "325c03f8074c54728cf9",
+    "url": "/MindMap/static/js/main.4df8188f.chunk.js"
+  },
+  {
+    "revision": "3fa51997672f9cc06bb5",
+    "url": "/MindMap/static/js/runtime~main.107752d2.js"
+  }
+]);
